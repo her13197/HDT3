e2181dcf5aeef83729491c5057b4d52ddf13092f
usuario <usuario@172.17.14.188>
2014-07-25 07:36:16 -0600

    new commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+Autor: Erick Hernandez    Carné: 13197
+Seccion: 30
+
+Nombre de archivo: Main.java 
+Descripcion: Clase principal del programa; clase que se compila para iniciar.
+*/
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
