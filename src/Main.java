6b08d5b6cd2c5ec49c97a294914ed3f0aace57e4
peilor <arg13028@uvg.edu.gt>
2014-07-25 07:58:13 -0600

    Cambio 1.0.0

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,11 @@ Nombre de archivo: Main.java
 Descripcion: Clase principal del programa; clase que se compila para iniciar.
 */
 public class Main {
-
     
     public static void main(String[] args) {
-        
+        for(int i=0; i<200; i++){
+            int valorEntero = (int)Math.floor(Math.random()*9);
+        }
     }
 
 }
