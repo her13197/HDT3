3d0edce7f60ee9851f2e1f73fb4c647a7c58faa4
peilor <arg13028@uvg.edu.gt>
2014-07-27 16:40:25 -0600

    Cambio 1.0.1

--- src/Bubble.java
+++ src/Bubble.java
@@ -6,9 +6,32 @@ Nombre de archivo: Bubble.java
 Descripcion: clase Bubble que 
 */
 public class Bubble{
-
-    public static void Sort (Comparable[] list){
-
+    public void Bubble(int arr[]) {
         
-   }
+        int tmp[] = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+            print(arr);
+            if (i < arr.length - 1) {
+                System.out.println(" ---- >>>  Tahap ke " + (i + 1) + " ");
+            } else {
+                System.out.println(" ---- >>>  Tahap ke " + (i + 1) + " ----->>>> " + " Hasil Setelah buble sort ");
+
+            }
+        }
+    }
+
+    public void print(int a[]) {
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "  ");
+        }
+    }
 }
