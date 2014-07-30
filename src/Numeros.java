566aafd723114d09b46ce5c62c73c67ce83fe4b6
usuario <usuario@172.17.14.188>
2014-07-29 22:34:49 -0600

    new commit

---
+++ src/Numeros.java
@@ -0,0 +1,10 @@
+/*
+Autor: Erick Hernandez    Carné: 13197
+Seccion: 21
+
+Nombre de archivo: Numeros.java 
+Descripcion: clase Numeros que 
+*/
+public class Numeros{
+
+}
---
+++ src/Sort.java
@@ -0,0 +1,12 @@
+/*
+Autor: Erick Hernandez    Carné: 13197
+Seccion: 21
+
+Nombre de archivo: Sort.java 
+Descripcion: clase Sort que 
+*/
+public interface Sort<E>{
+
+    public void Sort(Comparable[] list);
+    
+}
